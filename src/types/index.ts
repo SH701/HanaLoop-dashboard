85bd87d67d6139c9fbee0b9f156a030f032fa18b
KSH701 <rkrk7636@naver.com>
2026-05-19 17:43:20 +0900

    chore: 프로젝트 초기 세팅 - 타입 정의, 시드 데이터, Fake API

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export type Country = {
+  code: string;
+  name: string;
+};
+
+export type GhgEmission = {
+  yearMonth: string;
+  source: string;
+  emissions: number;
+};
+
+export type Company = {
+  id: string;
+  name: string;
+  country: string;
+  emissions: GhgEmission[];
+};
+
+export type Post = {
+  id: string;
+  title: string;
+  resourceUid: string;
+  dateTime: string;
+  content: string;
+};
